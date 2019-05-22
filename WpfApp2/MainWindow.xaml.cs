c48868b8dd520e7a79d27a37236469adc2997079
kurigohan0 <huihurma322@yandex.ru>
2019-05-22 20:18:23 +0300

    Fixes

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* 3-1-10
+В текстовом файле хранится информация в формате CSV, выведите ее в ListView. Запрещено исполь-
+зовать специализированные функции для формата CSV.
+Насонов Е. гр. 205
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
@@ -8,6 +15,7 @@ using Binding = System.Windows.Data.Binding;
 using System.IO;
 using MessageBox = System.Windows.MessageBox;
 using Button = System.Windows.Controls.Button;
+using System.Text.RegularExpressions;
 
 namespace WpfApp2
 {
@@ -17,19 +25,23 @@ namespace WpfApp2
 
     public partial class MainWindow : Window
     {
-        private char delimiter;
+        private char delimiter; //Знак, через который идет разделение
 
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Обработчик нажатия кнопок
+        /// </summary>
+        /// <param name="sender">Обьект кнопки</param>
+        /// <param name="e">Информация события</param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             var button = (Button)sender;
             try
             {
-                if (button.Tag.ToString() == "BrowseButton")
+                if (button.Tag.ToString() == "BrowseButton") //Кнопка обзора
                 {
                     OpenFileDialog openFileDialog1 = new OpenFileDialog
                     {
@@ -40,25 +52,34 @@ namespace WpfApp2
                     openFileDialog1.ShowDialog();
                     textBox1.Text = openFileDialog1.FileName;
                 }
-                else
+                else //Кнопка отображения таблицы
                 {
                     MainListView.Items.Clear();
 
                     delimiter = char.Parse(delimiter_box.Text);
                     string fileRow;
                     string[] fileDataField;
-                    List<string> list = new List<string>();
 
                     var gridView = new GridView();
+                    List<string> list = new List<string>();
+
                     System.IO.StreamReader fileReader = new StreamReader(textBox1.Text);
                     while (fileReader.Peek() != -1)
                     {
+
                         fileRow = fileReader.ReadLine();
-                        fileDataField = fileRow.Split(delimiter);
+                        
+                        fileDataField = Regex.Split(fileRow, "(?:^|,)(\"(?:[^\"]+|\"\")*\"|[^,]*)");
                         list = fileDataField.ToList<string>();
+                        
+                        for (int i = 0; i < list.Count; i++)
+                        {
+                            list.Remove("");
+                        }
 
                         for (int i = 0; i < list.Count; i++)
                         {
+                            list[i] = list[i].Trim(new char[] { '"' });
                             if (gridView.Columns.Count != list.Count)
                             {
                                 this.MainListView.View = gridView;
@@ -89,6 +110,10 @@ namespace WpfApp2
             catch (DirectoryNotFoundException)
             {
                 MessageBox.Show("Путь до выбранного вами файла неверен.");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Файл не может быть прочитан.");
             }
         }
     }
